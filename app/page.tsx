c15e8a32ea9de0758afc115076fa6c71ec964e31
Tanut-M2 <tanat44@gmail.com>
2026-03-28 14:14:16 +0100

    add table

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { TableDemo } from "@/src/TableDemo";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <TableDemo />
+    </>
+  );
 }
